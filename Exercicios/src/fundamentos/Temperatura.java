72273c854c9e1c408c366dc2507c1652d6a63090
Eric-Leal <ericgleal@gmail.com>
2024-12-20 15:59:09 -0300

    Mais matéria

--- Exercicios/src/fundamentos/Temperatura.java
+++ Exercicios/src/fundamentos/Temperatura.java
@@ -8,12 +8,11 @@ public class Temperatura {
 		final double FATOR = 5.0/9.0;
 		
 		double fahrenheit = 130.0;
-		double celsius = (fahrenheit - AJUSTE) * FATOR;;
+		double celsius = (fahrenheit - AJUSTE) * FATOR;
 		System.out.println("A temperatura em celsius é de :" + celsius);
 		
-		
 		fahrenheit = 86.0;
-		celsius = (fahrenheit - AJUSTE) * FATOR;;
+		celsius = (fahrenheit - AJUSTE) * FATOR;
 		System.out.println("A temperatura em celsius é de :" + celsius);
 		
 	}
