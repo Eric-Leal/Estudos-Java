90719cfac56a7f85d341c7b771d4cebb3135dc88
Eric-Leal <ericgleal@gmail.com>
2025-01-08 11:12:25 -0300

    Commit

--- Exercicios/src/classe/ProdutoTeste.java
+++ Exercicios/src/classe/ProdutoTeste.java
@@ -4,7 +4,7 @@ public class ProdutoTeste {
 
 	public static void main(String[] args) {
 		
-		Produto p1 = new Produto("Notebook", 4356.99, 0.25);
+		Produto p1 = new Produto("Notebook", 4356.99);
 		//p1.nome = "Notebook";
 		//p1.preco = 4356.99;
 		//p1.desconto = 0.25;
@@ -12,19 +12,20 @@ public class ProdutoTeste {
 		var p2 = new Produto();
 		p2.nome = "Caneta";
 		p2.preco = 12.56;
-		p2.desconto = 0.29;
 		
-		System.out.println(p2.nome);
+		
+		
 		System.out.println(p1.nome);
 		
 		//Nesse exemplo ele usa o metodo que esta com nenhum parametro, com os parenteses vazios
 		double precoFinal1 = p1.precoComDesconto();
 		
 		//Nesse ele usa o metodo que tem o parametro de 'double descontoDoGerente'
-		double precoFinal2 = p1.precoComDesconto(0.1); 
+		double precoFinal2 = p2.precoComDesconto(0.1); 
 		double mediaCarrinho = ((precoFinal1 + precoFinal2) / 2);
 		
 		System.out.println(precoFinal1);
+		System.out.println(p2.nome);
 		System.out.println(precoFinal2);
 		System.out.printf("Média do carrinho = %.2f", mediaCarrinho);
 		
