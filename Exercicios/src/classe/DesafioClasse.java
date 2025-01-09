ce82c06d9e7e578bed8d33fc1f8e53189f62ef36
Eric-Leal <ericgleal@gmail.com>
2025-01-09 11:46:42 -0300

    Mais commit

---
+++ Exercicios/src/classe/DesafioClasse.java
@@ -0,0 +1,17 @@
+package classe;
+
+public class DesafioClasse {
+
+	int a = 3;
+	// OU
+	static int b = 4;
+	
+	public static void main(String[] args) {
+		
+		DesafioClasse p = new DesafioClasse();
+		System.out.println(p.a);
+		// OU
+		System.out.println(b);
+		
+	}
+}
